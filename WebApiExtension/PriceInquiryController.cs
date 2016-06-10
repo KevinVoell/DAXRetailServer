076a0ce91d6af64f046188117eab2d8c21012347
Kevin Voell <kevin.voell@rsmus.com>
2016-06-10 15:26:57 -0600

    Controller refactoring

--- WebApiExtension/PriceInquiryController.cs
+++ WebApiExtension/PriceInquiryController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.Dynamics.Commerce.Runtime;
+using Microsoft.Dynamics.Commerce.Runtime.Client;
+using Microsoft.Dynamics.Commerce.Runtime.Configuration;
+using Microsoft.Dynamics.Commerce.Runtime.Data;
 using Microsoft.Dynamics.Commerce.Runtime.DataModel;
 using Microsoft.Dynamics.Retail.RetailServerLibrary;
 using System.Runtime.InteropServices;
+using System.Web.Http;
 
 namespace Retail.Server.Extensions.Example.WebApiExtension
 {
@@ -11,15 +15,18 @@ namespace Retail.Server.Extensions.Example.WebApiExtension
     [ComVisible(false)]
     [ExtendedController("PriceInquiry")]
     [CommerceAuthorization(AllowedRetailRoles = new string[] { CommerceRoles.Anonymous }, CheckRetailOperation = false, DeviceTokenRequired = false)]
-    public class PriceInquiryController : RetailServerApiController
+    public class PriceInquiryController : ApiController
     {
+        protected CommerceIdentity CommerceIdentity;
+
         /// <summary>
         /// Default constructor
         /// </summary>
         public PriceInquiryController()
         {
             // Need to set the ChannelId explicitly
-            this.CommerceIdentity.ChannelId = 12345678;
+            this.CommerceIdentity = User.Identity as CommerceIdentity;
+            this.CommerceIdentity.ChannelId = 5637144662;
         }
 
         /// <summary>
@@ -44,7 +51,7 @@ namespace Retail.Server.Extensions.Example.WebApiExtension
                 ItemIds = new[] { new ProductLookupClause(itemId) }
             };
 
-            var products = this.productManager.SearchProducts(searchCriteria, new QueryResultSettings(new PagingInfo(1, 0)));
+            var products = GetProductManager().SearchProducts(searchCriteria, new QueryResultSettings(new PagingInfo(1, 0)));
 
             if (products.Results.Count > 0)
             {
@@ -52,6 +59,20 @@ namespace Retail.Server.Extensions.Example.WebApiExtension
             }
 
             return -1;
+        }
+
+        private ProductManager GetProductManager()
+        {
+            var connectionString = System.Configuration.ConfigurationManager.AppSettings["ConnectionString"];
+            var configurationSection = CommerceRuntimeConfigurationManager.GetConfigurationSection(CommerceRuntimeConfigurationManager.SectionName);
+            var configuration = new CommerceRuntimeConfiguration(configurationSection, connectionString);
+
+            var principal = new CommercePrincipal(this.CommerceIdentity);
+
+            var runtime = CommerceRuntime.Create(configuration, principal);
+            var pm = ProductManager.Create(runtime);
+
+            return pm;
         }
     }
 }
---
+++ WebApiExtension/RealTimeServiceController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime.Data;
+using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+using Microsoft.Dynamics.Commerce.Runtime.Services.Messages;
+using Microsoft.Dynamics.Commerce.Runtime.TransactionService;
+using Microsoft.Dynamics.Retail.RetailServerLibrary;
+using System.Runtime.InteropServices;
+using System.Web.Http;
+
+namespace Retail.Server.Extensions.Example.WebApiExtension
+{
+    /// <summary>
+    /// WebApi controller for real time service call to AX.
+    /// </summary>
+    [ComVisible(false)]
+    [ExtendedController("DAXVersionRealTimeService")]
+    [CommerceAuthorization(AllowedRetailRoles = new string[] { CommerceRoles.Employee }, CheckRetailOperation = false, DeviceTokenRequired = false)]
+    public class DAXVersionRealTimeServiceController : ApiController
+    {
+        protected CommerceIdentity CommerceIdentity;
+
+        private TransactionServiceClient TransactionServiceClient;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DAXVersionRealTimeServiceController()
+        {
+            // Need to set the ChannelId explicitly
+            this.CommerceIdentity = User.Identity as CommerceIdentity;
+            this.CommerceIdentity.ChannelId = 5637144662;
+
+            TransactionServiceClient = new TransactionServiceClient(CommerceRuntimeManager.Runtime.CreateRequestContext(new RealtimeRequest()));
+        }
+
+        /// <summary>
+        /// Gets the adjusted price for the specified itemId.
+        /// </summary>
+        /// <param name="itemId">The ItemId to find the price for.</param>
+        /// <returns>The price or null if item is not found.</returns>
+        /// <remarks>
+        /// Called:
+        /// http://[RetailServer]:8305/RetailServer/v1/api/DAXVersionRealTimeService
+        /// </remarks>
+        public string Get()
+        {
+            var version = "Unknown";
+
+            var result = this.TransactionServiceClient.InvokeExtensionMethod("axVersion", new object[] { });
+
+            
+            var success = (bool)result[0];
+
+            if (success)
+            {
+                version = (string)result[1];
+            }
+
+            return version;
+        }
+    }
+
+    /// <summary>
+    /// Concrete class needed to instantiate a <c>ServiceRequest</c>.
+    /// </summary>
+    internal class RealtimeRequest : ServiceRequest
+    {
+    }
+}
